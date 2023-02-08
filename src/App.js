761b15dc0d2c108eefa7deaf5bf0c9631583b2ca
Sandeep1203tech <sandeepsahoo865@gmail.com>
2023-02-08 11:29:51 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import SideNavBar from "./SideNavBar/SideNavBar";
+import Home from "./Home/Home";
+import "./App.css"
+
+function App() {
+	return (
+		<div className="App">
+      <Home/>
+			<SideNavBar />
+		</div>
+    
+	);
+}
+
+export default App;
