28ef0f94258acea32f2f2a8837e2b919363ef3f1
Sandeep1203tech <sandeepsahoo865@gmail.com>
2023-02-09 20:13:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import SideNavBar from "./SideNavBar/SideNavBar";
 import Home from "./Home/Home";
-import "./App.css"
+import "./App.css";
 import About from "./About/About";
-// import SearchBar from "./SearchBar/SearchBar";
-
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-	
-	return (
-		<div className="App">
-			<SideNavBar />
+  return (
+    <div className="App">
+      {/* <SideNavBar />
 			<Home />
-			<About />
-			{/* <About />
+			<About /> */}
+      {/* <About />
 			<About />
 			<About /> */}
-			{/* <SearchBar/> */}
-		</div>
-
-	);
+      {/* <SearchBar/> */}
+	  
+      {/* <Routes>
+        <Route path="/" element={<Home/>} />
+		<Route path="/SideNavBar" element={<SideNavBar />} />
+        <Route path="/About" element={<About />} />
+      </Routes> */}
+    </div>
+  );
 }
 
 export default App;
+
+// import React from "react";
+// import { Route, Routes } from "react-router-dom";
+// // import EE from './EE/EE';
+// // import Contact from './Contact/Contact';
+// import Home from './Home/Home';
+// import About from "./About/About";
+
+// const App = () => {
+// 	return (
+// 		<>
+//          <Routes>
+// 	          <Route path = '/' element={<Home/>} />
+// 			  <Route path = '/About' element={<About/>} />
+//          </Routes>
+
+// 		</>
+// 	);
+// };
+
+// export default App;
