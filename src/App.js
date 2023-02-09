e9ee6e3250deb3b5d221243de429cee3190a0479
Sandeep1203tech <sandeepsahoo865@gmail.com>
2023-02-10 00:08:05 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -2,47 +2,32 @@ import SideNavBar from "./SideNavBar/SideNavBar";
 import Home from "./Home/Home";
 import "./App.css";
 import About from "./About/About";
-import { Route, Routes } from "react-router-dom";
+import ErrorPage from "./ErrorPage/ErrorPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+      errorElement: <ErrorPage />,
+    }
+  ]);
   return (
     <div className="App">
-      {/* <SideNavBar />
-			<Home />
-			<About /> */}
-      {/* <About />
-			<About />
-			<About /> */}
-      {/* <SearchBar/> */}
-	  
-      {/* <Routes>
-        <Route path="/" element={<Home/>} />
-		<Route path="/SideNavBar" element={<SideNavBar />} />
-        <Route path="/About" element={<About />} />
-      </Routes> */}
+      <SideNavBar />
+      <RouterProvider router={router} />
     </div>
   );
 }
 
 export default App;
-
-// import React from "react";
-// import { Route, Routes } from "react-router-dom";
-// // import EE from './EE/EE';
-// // import Contact from './Contact/Contact';
-// import Home from './Home/Home';
-// import About from "./About/About";
-
-// const App = () => {
-// 	return (
-// 		<>
-//          <Routes>
-// 	          <Route path = '/' element={<Home/>} />
-// 			  <Route path = '/About' element={<About/>} />
-//          </Routes>
-
-// 		</>
-// 	);
-// };
-
-// export default App;
